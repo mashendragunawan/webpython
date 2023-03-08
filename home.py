793f9bce1e783ebedd4fb37674ec227241b47e17
mashendragunawan <mashendragunawan04@gmail.com>
2023-03-08 16:37:16 +0700

    Home

--- home.py
+++ home.py
@@ -1,9 +1,2 @@
 print("hello world")
 
-print("membuat project di github memang susah mas brooo")
-
-print ("saya juga belajar tentang banyak hal di bumi ini termasuk bahasa pemograman python")
-
-print("aku senang banget telah berhasil membuat push pada github walaupun aku tidak begitu paham dengan apa yanng saya lakukan")
- 
-print("Aku Curhat Dikit ya")

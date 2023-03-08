e5b4f717c0b67462541e1f1134fe04772b23f966
mashendragunawan <mashendragunawan04@gmail.com>
2023-03-08 16:36:18 +0700

    Frest

--- home.py
+++ home.py
@@ -5,3 +5,5 @@ print("membuat project di github memang susah mas brooo")
 print ("saya juga belajar tentang banyak hal di bumi ini termasuk bahasa pemograman python")
 
 print("aku senang banget telah berhasil membuat push pada github walaupun aku tidak begitu paham dengan apa yanng saya lakukan")
+ 
+print("Aku Curhat Dikit ya")

84d5a8eb7cf1b8eb8181f64028ea3e3b9e9cb1d4
mashendragunawan <mashendragunawan04@gmail.com>
2023-03-09 16:02:24 +0700

    bisa

--- home.py
+++ home.py
@@ -1,4 +1,10 @@
-nama = input("Siapa Nama Mu:")
-umur = input("Brapa Umurmu :")
-print ("Hello",nama,"umur kamu adalah", umur,"tahun")
+#nama = input("Siapa Nama Mu:")
+#umur = input("Brapa Umurmu :")
+#print ("Hello",nama,"umur kamu adalah", umur,"tahun")
 
+angka1 = int(input("Maukan Angka Pertama="))
+angka2 = int(input("masukan angka kedua="))
+
+hasil= angka1+angka2 
+
+print("Hail Penjumlahan dari",angka1,"+",angka2,"adalah",hasil)

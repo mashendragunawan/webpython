f3d8174e28a0954e7a532b6e772a61109d5afa78
mashendragunawan <mashendragunawan04@gmail.com>
2023-03-15 13:25:41 +0700

    test aja web Python

--- we.py
+++ we.py
@@ -1,9 +1,9 @@
 from flask import Flask
 
-app = Flask(__name__)
+tt = Flask(__name__)
 
-@app.route('/')
+@tt.route('/')
 def index():
-    return 'Akhirnya Aku Bisa Juga MENAMPILKAN WEB dengan python'
+    return'hello'
 
-app.run(host='0.0.0.0', port=229)
+tt.run(host='0.0.0.0', port=229)

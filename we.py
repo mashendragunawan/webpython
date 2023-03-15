817621b0b5a9e9453c3bebebf9f58ff593988774
mashendragunawan <mashendragunawan04@gmail.com>
2023-03-15 12:30:39 +0700

    TEST

--- we.py
+++ we.py
@@ -1,6 +1,9 @@
-from flask import Flask
-app = Flask(__name__)@app.route("/")
-def main():
-    return "Welcome!" 
-if __name__ == "__main__":
-    app.run()
+from flask import Flask, render_template
+app = Flask(__name__)
+
+if(__name__=="__main__") :
+    app.run(debug = True)
+
+@app.route("/")
+def welcome() :
+    return "welcome to my web"
